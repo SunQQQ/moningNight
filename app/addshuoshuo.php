1b178d42e1619cc5874d9dffe72c520e6f7a0d8b
OnlyMid-sunquan <1585437938>
2017-09-06 17:06:47 +0800

    生成的图片还是不正常

--- app/addshuoshuo.php
+++ app/addshuoshuo.php
@@ -1,11 +1,21 @@
 <?php
-$pic = isset($_POST["picInput"])? $_POST["picInput"] : 'test';
+if(isset($_POST["picInput"])){
+    $pic = base64_decode($_POST["picInput"]);
+}
 $shuoshuoTxt = $_POST["txtInput"];
+
+// 生成的文件名
+$photo = "2.jpg";
+
+// 生成文件
+file_put_contents($photo, $pic, true);
 
 //move_uploaded_file($pic,"../pic/shuoshuo/".$_FILES["picdata"]["name"]);
 //if($_FILES["picdata"]["name"]){
 //    $picUrl = "pic/shuoshuo/".$_FILES["picdata"]["name"];
 //}
+move_uploaded_file($photo,"../pic/shuoshuo/1.png");
+$picUrl = "pic/shuoshuo/1.png";
 
 $connent = new mysqli("localhost","root","","moningnight");
 if($connent->connect_error){
@@ -13,12 +23,12 @@ if($connent->connect_error){
 }else{
 }
 
-//$insertdata = "insert into shuoshuo(shuoshuo,pic) values('".$shuoshuoTxt."','".$picUrl."')";
-$insertdata = "insert into shuoshuo(shuoshuo,pic) values('".$shuoshuoTxt."','".$pic."')";
+$insertdata = "insert into shuoshuo(shuoshuo,pic) values('".$shuoshuoTxt."','".$picUrl."')";
+//$insertdata = "insert into shuoshuo(shuoshuo,pic) values('".$shuoshuoTxt."','".$pic."')";
 
 $json = [
     "code" => "200",
-    "jsondata" => "a"
+    "jsondata" => $pic
 ];
 echo json_encode($json,JSON_UNESCAPED_UNICODE);
 if($connent->query($insertdata)==true){
